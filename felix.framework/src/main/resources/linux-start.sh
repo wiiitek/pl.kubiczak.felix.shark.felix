9225954a6e5eac694dc7e920315a449ef3412c81
Wiiitek <kubiczak.jakub@gmail.com>
2017-03-05 14:49:04 +0000

    update for linux  start script to be similar to the OPENSHIFT one

--- felix.framework/src/main/resources/linux-start.sh
+++ felix.framework/src/main/resources/linux-start.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-JAVA_IO_TMPDIR=./felix-cache/io_tmp
+SHARK_DIR=${SHARK_HOME}/felix-framework
+TMP_DIR=${SHARK_DIR}/felix-cache
+TODAY=$(date +"%Y-%m-%d")
+LOG_FILE=${SHARK_DIR}/logs/framework_${TODAY}.log
 
-rm -fr $JAVA_IO_TMPDIR
+rm -fr ${TMP_DIR}/io_tmp
+echo "JAVA io tmp folder deleted"
+rm -fr ${TMP_DIR}/bundles
+echo "Felix bundles cache deleted"
 
 DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=30303"
-LOGGING_OPTS="-Dlogback.configurationFile=./conf/logback-dev.xml"
-JAVA_OPTS="-Djava.io.tmpdir=$JAVA_IO_TMPDIR -Dfile.encoding=UTF-8"
+echo "debug options: '${DEBUG_OPTS}'"
+JAVA_OPTS="-Djava.io.tmpdir=${TMP_DIR}/io_tmp -Dfile.encoding=UTF-8"
+echo "java options: '${JAVA_OPTS}'"
+LOGGING_OPTS="-Dlogback.configurationFile=${SHARK_DIR}/conf/logback-dev.xml"
+echo "logging options: '${LOGGING_OPTS}'"
+FELIX_OPTS="-Dgosh.args=--nointeractive -Dorg.osgi.framework.storage=${TMP_DIR}/bundles"
+# host
+FELIX_OPTS="${FELIX_OPTS} -Dorg.apache.felix.http.host=0.0.0.0"
+# port
+FELIX_OPTS="${FELIX_OPTS} -Dorg.osgi.service.http.port=8080"
+# webconsole username and password
+FELIX_OPTS="${FELIX_OPTS} -Dfelix.webconsole.username=admin -Dfelix.webconsole.password=admin"
+echo "felix options: '${FELIX_OPTS}'"
 
-PATH=$JAVA_HOME/bin:$PATH
+cd ${SHARK_DIR}
+echo "folder changed to: '$(pwd)'"
 
-java $DEBUG_OPTS $LOGGING_OPTS $JAVA_OPTS -jar bin/felix.jar
+COMMAND="java ${DEBUG_OPTS} ${JAVA_OPTS} ${LOGGING_OPTS} ${FELIX_OPTS} -jar bin/felix.jar"
+echo "running '${COMMAND}'"
+
+# running with nohup
+# see
+# https://www.cyberciti.biz/tips/nohup-execute-commands-after-you-exit-from-a-shell-prompt.html
+nohup ${COMMAND} >> ${LOG_FILE} 2>&1&
